022759e472fb6591835bd25307acb6c4ef12db69
Avinash <avinashverma.av46@gmail.com>
2023-09-19 10:48:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import data from "./data.json";
+
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="templateContainer">
+        <div id="main_title">
+     <h4>Your Favourite Game Of Thrones Character</h4>
+        </div>
+        <div className="searchInput_Container">
+          
+          <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div>
+        <div className="template_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm == ""){
+                  return val;
+                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }
+              })
+              .map((val) => {
+                return(
+                  <div className="template" key={val.id}>
+                      <img src={val.image} alt="" />
+                      <h3>{val.title}</h3>
+                      <p className="House">House:{val.House}</p>
+                  </div> 
+                )
+              })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
